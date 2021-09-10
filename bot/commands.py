2eee9194f3e86f0b72498fd9cbccfd0076bc75af
Gabben <43146729+gabbhack@users.noreply.github.com>
2021-09-10 22:24:09 +0500

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -4,21 +4,23 @@ from discord.ext import commands
 
 from .core import img_api
 
+ANIMALS = ['dog', 'cat', 'panda', 'red_panda', 'birb', 'fox', 'koala']
+
 @commands.command()
 async def img(ctx, arg=None):
     if arg is None:
         await ctx.send("Используй команду так: /img <животное>.\nЗнаю таких животных: dog, cat, panda, red_panda, birb, fox, koala")
         return
+
+    if arg not in ANIMALS:
+        await ctx.send("Не знаю такого животного. Знаю этих: dog, cat, panda, red_panda, birb, fox, koala")
+        return
+
     try:
         image = await img_api.get(arg)
         embed = Embed(title=f"Random {arg}")
         embed.set_image(url=image.link)
         await ctx.send(embed=embed)
-    except ClientResponseError as response:
-        if response.status == 404:
-            await ctx.send("Не знаю такого животного. Знаю этих: dog, cat, panda, red_panda, birb, fox, koala")
-        else:
-            await ctx.send("Произошла неизвестная ошибка :(")
     except Exception:
         await ctx.send("Произошла неизвестная ошибка :(")
 
